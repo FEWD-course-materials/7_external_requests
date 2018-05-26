ea4f1b12e2fe439feb0fabc148fb0ff81ee6d568
terence lim <terencelim92@gmail.com>
2018-05-26 14:39:23 +0800

    Solution

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-// Random quote API
-// https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&
+$(document).ready(function() {
+    generateQuote();
+
+    $(".generate-quote").click(generateQuote);
+
+    function generateQuote() {
+        $.ajax({
+            url: "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?",
+            dataType: "json",
+            method: "GET",
+        }).done(function(data) {
+            $(".quote").html(data.quoteText);
+            $(".author").html(data.quoteAuthor);
+        });
+    };
+})
